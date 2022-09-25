0fffacb29e6df4f93c3223b36962778bcd5b71e3
tranphananh1001 <tranphananh1001@gmail.com>
2022-09-25 10:08:45 +0700

    authen_by_OAuth: update code

--- README.md
+++ README.md
@@ -13,3 +13,5 @@ https://newdevzone.com/posts/how-to-replace-all-html-tag-with-empty-string-in-go
 https://pkg.go.dev/golang.org/x/net/html
 
 https://developers.google.com/gmail/api/reference/rest/v1/users.messages/get
+
+https://pkg.go.dev/encoding/csv
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/base64"
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"log"
@@ -10,7 +11,6 @@ import (
 	"os"
 	"strings"
 
-	"github.com/microcosm-cc/bluemonday"
 	"golang.org/x/net/html"
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/google"
@@ -73,6 +73,177 @@ func saveToken(path string, token *oauth2.Token) {
 	json.NewEncoder(f).Encode(token)
 }
 
+type Item struct {
+	TimeReceive  string
+	ItemName     string
+	ItemId       string
+	ItemQuantity string
+	TrackingId   string
+	ShipAdd      string
+}
+
+const User = "me"
+
+var HeaderCSV = []string{
+	"Date & Time Received", "Name", "Item ID", "Quantity", "Tracking ID", "Ship To",
+}
+
+func getMailItems(srv *gmail.Service, mail []*gmail.Message) []Item {
+	var items []Item
+	for _, m := range mail {
+		fmt.Printf("%v\n", m.Id)
+		messageResponse, err := srv.Users.Messages.Get(User, string(m.Id)).Do()
+		if err != nil {
+			log.Fatalf("Get Messages Error: %v", err)
+		}
+
+		var timeReceive string
+		for _, v := range messageResponse.Payload.Headers {
+			if v.Name == "Date" {
+				timeReceive = v.Value
+			}
+		}
+
+		dataEncode := (messageResponse.Payload.Parts[1].Body.Data)
+		data, err := base64.URLEncoding.DecodeString(dataEncode)
+		if err != nil {
+			log.Fatalf("DecodeString Error: %v", err)
+		}
+
+		z := html.NewTokenizer(strings.NewReader(string(data)))
+		const NameStyle = "font-family:Helvetica;font-size:12px;font-weight:700;letter-spacing:0.25;line-height:18px;text-align:left;color:#0A0A0A;"
+		const ItemIdStyle = "font-family:Helvetica;font-size:12px;font-weight:400;letter-spacing:0.25;line-height:18px;text-align:left;color:#000000;"
+		const QuantityStyle = "font-family:Helvetica;font-size:12px;font-weight:400;letter-spacing:0.25;line-height:18px;text-align:center;color:#4D4D4D;"
+
+		var trackingId string
+		// var orderDate string
+		var shipAdd string
+		var itemIds []string
+		var itemNames []string
+		var itemQuantites []string
+
+		end := false
+		for {
+			if end {
+				break
+			}
+			tt := z.Next()
+			switch tt {
+			case html.ErrorToken:
+				fmt.Println("End")
+				end = true
+
+			case html.StartTagToken:
+				t := z.Token()
+				if t.Data != "div" {
+					continue
+				}
+				attrs := t.Attr
+				for _, v := range attrs {
+					if v.Key == "style" && v.Val == ItemIdStyle {
+						z.Next()
+						t := z.Token()
+						itemId := strings.ReplaceAll(t.Data, "ITEM", "")
+						itemId = strings.TrimSpace(itemId)
+						itemIds = append(itemIds, itemId)
+					}
+
+					if v.Key == "style" && v.Val == NameStyle {
+						z.Next()
+						t := z.Token()
+						// fmt.Printf("%T %v \n", t.Data, t.Data)
+						itemNames = append(itemNames, t.Data)
+					}
+
+					if v.Key == "style" && v.Val == QuantityStyle {
+						z.Next()
+						t := z.Token()
+						itemQuantites = append(itemQuantites, t.Data)
+					}
+				}
+
+			case html.TextToken:
+				t := z.Token()
+				htmlText := strings.TrimSpace(t.Data)
+				if htmlText == "" {
+					continue
+				}
+				if htmlText == "TRACKING #:" {
+					z.Next()
+					z.Next()
+					t = z.Token()
+					trackingId = t.Data
+					// fmt.Printf("trackingId %T %v \n", trackingId, trackingId)
+					continue
+				}
+				// if strings.Contains(htmlText, "ORDER DATE:") {
+				// 	orderDate = strings.ReplaceAll(htmlText, "ORDER DATE:", "")
+				// 	orderDate = strings.TrimSpace(orderDate)
+				// 	// fmt.Printf("orderDate %T %v \n", orderDate, orderDate)
+				// 	continue
+				// }
+				if htmlText == "SHIP TO:" {
+					z.Next()
+					z.Next()
+					z.Next()
+					z.Next()
+					t = z.Token()
+					shipAdd = strings.TrimSpace(t.Data)
+					z.Next()
+					z.Next()
+					t = z.Token()
+					shipAdd += "\n" + strings.TrimSpace(t.Data)
+					// fmt.Printf("shipAdd %T %v \n", shipAdd, shipAdd)
+					continue
+				}
+			}
+		}
+
+		for i, _ := range itemIds {
+			items = append(items, Item{
+				TimeReceive:  timeReceive,
+				ItemName:     itemNames[i],
+				ItemId:       itemIds[i],
+				ItemQuantity: itemQuantites[i],
+				TrackingId:   trackingId,
+				ShipAdd:      shipAdd,
+			})
+		}
+		// fmt.Printf("%T %v \n", trackingId, trackingId)
+		// fmt.Printf("%T %v \n", shipAdd, shipAdd)
+
+		// fmt.Printf("%T %v \n", itemNames, itemNames)
+		// fmt.Printf("%T %v \n", itemIds, itemIds)
+		// fmt.Printf("%T %v \n", itemQuantites, itemQuantites)
+	}
+	return items
+}
+
+func exportCsv(items []Item) {
+	csvFile, err := os.Create("items.csv")
+
+	if err != nil {
+		log.Fatalf("failed creating file: %s", err)
+	}
+
+	csvwriter := csv.NewWriter(csvFile)
+	_ = csvwriter.Write(HeaderCSV)
+
+	for _, item := range items {
+		row := []string{
+			item.TimeReceive,
+			item.ItemName,
+			item.ItemId,
+			item.ItemQuantity,
+			item.TrackingId,
+			item.ShipAdd,
+		}
+		_ = csvwriter.Write(row)
+	}
+	csvwriter.Flush()
+	csvFile.Close()
+}
+
 func main() {
 	ctx := context.Background()
 	b, err := os.ReadFile("credentials.json")
@@ -92,8 +263,11 @@ func main() {
 		log.Fatalf("Unable to retrieve Gmail client: %v", err)
 	}
 
-	user := "me"
-	r, err := srv.Users.Messages.List(user).MaxResults(1).Q("label:sephora-arrived").Do()
+	// Change Label
+	r, err := srv.Users.Messages.List(User).Q("label:sephora-arrived").Do()
+	// Change Limit
+	// r, err := srv.Users.Messages.List(User).MaxResults(2).Q("label:sephora-arrived").Do()
+
 	if err != nil {
 		log.Fatalf("Unable to retrieve Messages: %v", err)
 	}
@@ -104,70 +278,8 @@ func main() {
 
 	// email := "trunidojoan@gmail.com"
 	fmt.Println("Messages:")
-	for _, m := range r.Messages {
-		fmt.Printf("%v\n", m.Id)
-		messageResponse, err := srv.Users.Messages.Get(user, string(m.Id)).Do()
-		if err != nil {
-			log.Fatalf("Get Messages Error: %v", err)
-		}
-		dataEncode := (messageResponse.Payload.Parts[1].Body.Data)
-		data, err := base64.URLEncoding.DecodeString(dataEncode)
-		if err != nil {
-			log.Fatalf("DecodeString Error: %v", err)
-		}
 
-		z := html.NewTokenizer(strings.NewReader(string(data)))
-		i := 0
-		var isTracking bool
-		var isShipTo bool
-		for {
-			i++
-			tt := z.Next()
-			switch tt {
-			case html.ErrorToken:
-				fmt.Println("End")
-				return
+	items := getMailItems(srv, r.Messages)
 
-			case html.StartTagToken:
-				// t := z.Token()
-
-			case html.TextToken:
-				t := z.Token()
-				htmlText := strings.TrimSpace(t.Data)
-				if htmlText == "" {
-					continue
-				}
-				if htmlText == "TRACKING #:" {
-					isTracking = true
-					continue
-				}
-				if strings.Contains(htmlText, "ORDER DATE:") {
-					fmt.Println(htmlText)
-					continue
-				}
-				if htmlText == "SHIP TO:" {
-					isShipTo = true
-					continue
-				}
-
-				if isTracking {
-					fmt.Println(htmlText)
-				}
-				isTracking = false
-
-				if isShipTo {
-					fmt.Println(htmlText)
-				}
-				isShipTo = false
-			}
-
-		}
-
-		p := bluemonday.StripTagsPolicy()
-		text := p.Sanitize(string(data))
-		text = strings.Replace(text, "\n", " ", -1)
-		text = strings.Replace(text, "   ", "", -1)
-		text = strings.Replace(text, "  ", "", -1)
-		// fmt.Printf("%s", text)
-	}
+	exportCsv(items)
 }
